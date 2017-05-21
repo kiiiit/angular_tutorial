54141751f768aa54c1b488404de626086c77ad32
Kaito Seki <kit@kit-mac.local>
2017-05-21 14:09:43 +0900

    ignore編集

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -8,13 +8,13 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.name = "Angular";
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
-        selector: 'my-app',
+        selector: "my-app",
         template: "<h1>Hello {{name}}</h1>",
     })
 ], AppComponent);
